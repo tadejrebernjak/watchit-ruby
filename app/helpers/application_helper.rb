013c97f852dfc3cce9c3fbe4dfcb1f178727b4fd
tadejrebernjak <tadej.rebernjak2@gmail.com>
2021-12-02 13:43:33 +0100

    video list style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,11 @@ module ApplicationHelper
     def display_session_links
         capture do
             if user_signed_in?
-                concat link_to "Edit profile", edit_user_registration_path, class: 'btn btn-primary m-2 fs-3'
+                concat link_to 'Upload', new_video_path, class: 'btn btn-info m-2'
                 concat " "
-                concat link_to "Sign out", destroy_user_session_path, method: :delete, class: 'btn btn-danger m-2 fs-3'
+                concat link_to "Edit profile", edit_user_registration_path, class: 'btn btn-primary m-2'
+                concat " "
+                concat link_to "Sign out", destroy_user_session_path, method: :delete, class: 'btn btn-danger m-2'
             else
                 concat link_to "Sign in", new_user_session_path, class: 'btn btn-primary m-2'
                 concat " "
